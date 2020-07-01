c722bb6a08af34cfa4bf3388816b1e2ba2a80351
monia.taina <67027532+moniataina@users.noreply.github.com>
2020-07-01 10:33:54 -0300

    Update client.js

--- microprojeto-1/client.js
+++ microprojeto-1/client.js
@@ -1,1 +1,30 @@
 
+const net = require('net');
+
+// criação do socket
+const socket = net.Socket();
+
+
+
+//a partir daqui a função de conexão
+function handleConnection () {
+    
+    console.log("Sorveteria que delicia!\n");
+    console.log("bem vindo!\n");
+
+    // envio "MENSAGEM2" para o servidor
+
+    socket.write("MENSAGEM2");
+
+    
+    socket.on("data", function (dados) {
+        const resposta = dados.toString().trim();
+        console.log(resposta);
+
+        console.log("fim da conexão");
+        socket.end();
+    });
+}
+
+// conexão com o servidor
+socket.connect(2000, "127.0.0.1", handleConnection);
