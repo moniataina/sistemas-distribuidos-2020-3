25bbdddfefa59ba1929534363ca02ab2d149eb53
monia.taina <67027532+moniataina@users.noreply.github.com>
2020-07-01 10:51:50 -0300

    Update servidor.js

--- microprojeto-1/servidor.js
+++ microprojeto-1/servidor.js
@@ -1,1 +1,60 @@
+/*MONIA TAINA TAVARES
+Ciência da computação - UFMA
+com base do código https://github.com/rafaelfl/sockets-sd-2020-3/blob/master/src/client.js
+*/
 
+const net = require('net');
+var carrinho [];
+
+
+// função que trata todos os eventos da conexão no servidor
+function trataRequisicoes (socket) {
+    // imprime mensagem ao conectar
+    console.log("Conexão realizada!");
+
+    // código que executa quando a conexão é encerrada
+    socket.on("end", function () {
+        console.log("Conexão finalizada!");
+    });
+
+
+    function cardap(){
+        socket.write("Baunilha, coco, chocolate, flocos");
+
+    }
+    // dados sockets executados
+    socket.on("data", function (dados) {
+        const comando = dados.toString();
+            var opc = data.toString().split(" ");
+        // menu das opçoes
+        switch (opc[0]) {
+            case "1":
+                socket.write("sabores\n");
+                cardap();
+
+                break;
+
+            case "2":
+                socket.write("cadastrando dados\n")
+                console.log("cadastrado!\n")
+                carrinho
+;                break;
+            
+            default:
+                const c = comando.split(" ");
+
+              
+                    
+                } else {
+                    socket.write("opção invalida");
+                }
+                
+        }
+    });
+}
+
+// cria servidor
+const server = net.createServer(trataRequisicoes);
+
+// escuta em porta de rede
+server.listen(2000, "127.0.0.1");
