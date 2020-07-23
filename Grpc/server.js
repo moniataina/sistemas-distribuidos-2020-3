f84c0b2c15b46126596cf276fb206ca2975611ff
monia.taina <67027532+moniataina@users.noreply.github.com>
2020-07-23 08:14:36 -0300

    Update server.js

--- Grpc/server.js
+++ Grpc/server.js
@@ -1,19 +1,21 @@
-// definição do caminho do arquivo proto
-const PROTO_PATH = "./lanchonete.proto";
+//Monia Taina - ufma
+
+// def do caminho do arquivo proto
+const PROTO_PATH = "./sabor.proto";
 
 const grpc = require('grpc');
 
 const protoLoader = require('@grpc/proto-loader');
 
-// carregamento do arquivo proto e geração das definições
+//carregamento do caminho proto
 const packageDefinition = protoLoader.loadSync(
-	PROTO_PATH,
-	{keepCase: true,
-	 longs: String,
-	 enums: String,
-	 defaults: true,
-	 oneofs: true
-	});
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
 
-// carregamento do código do serviço
-var protoDescriptor = grpc.loadPackageDefinition(packageDefinition).lanch;
+//CODIGO DE SERVIÇO - CARREGAMENTO
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition).sabor;
